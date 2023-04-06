6997b4a3bb1ad9e92c5c4b0c0374ebdcf6c7ecde
Thành Tấn Trương <64310557+truongtanthanh1202@users.noreply.github.com>
2023-04-07 00:22:17 +0700

    update index.js in Login

--- components/screens/login/index.js
+++ components/screens/login/index.js
@@ -1,19 +1,151 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import Lock from '../../../assets/img/lock';
+import styles from './style';
+import Google from '../../../assets/icon/google';
 
 const Login = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#e4f1f9',
-      }}>
-      <Ionicons name="search-outline" size={32} />
-      <Text>Login </Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView style={{flex: 1, width: '100%'}}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View
+            style={{
+              flex: 1,
+              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Lock width="80" height="80"></Lock>
+
+            <Text style={styles.title}>Welcome Back</Text>
+            <Text style={styles.describe}>
+              A handful of model sentence structures
+            </Text>
+
+            <View style={{width: '100%'}}>
+              <TextInput
+                style={styles.inputField}
+                autoFocus={true}
+                placeholder="Email id"
+                placeholderTextColor="black"></TextInput>
+              <Ionicons
+                name="mail-unread-outline"
+                size={32}
+                style={styles.inputIcon}
+              />
+            </View>
+            <View style={{width: '100%'}}>
+              <TextInput
+                style={styles.inputField}
+                // autoFocus={true}
+                placeholder="Password"
+                placeholderTextColor="black"></TextInput>
+              <Ionicons
+                name="eye-off-outline"
+                size={32}
+                style={styles.inputIcon}
+              />
+            </View>
+
+            <TouchableOpacity
+              onPress={() => {
+                alert('Navigate to Forgot Password Screen');
+              }}>
+              <Text
+                style={{
+                  color: '#5297fe',
+                  textDecorationLine: 'underline',
+                  marginLeft: '68%',
+                  marginBottom: 24,
+                }}>
+                Forgot Password
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.buttonSignIn}>
+              <Text
+                style={{
+                  color: 'white',
+                  fontSize: 16,
+                  textAlign: 'center',
+                  fontFamily: 'Poppins-Medium',
+                  marginTop: 2,
+                }}>
+                Sign in
+              </Text>
+            </TouchableOpacity>
+
+            <Text
+              style={{
+                textAlign: 'center',
+                fontSize: 16,
+                fontWeight: 500,
+                marginBottom: 16,
+              }}>
+              ( or )
+            </Text>
+
+            <View
+              style={{
+                width: '80%',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Google width="28" height="28" style={styles.icon}></Google>
+              <TouchableOpacity
+                onPress={() => {
+                  alert('Login with Google');
+                }}>
+                <Text
+                  style={{
+                    margin: 8,
+                    textAlign: 'center',
+                    fontSize: 16,
+                    fontWeight: 600,
+                    marginLeft: 10,
+                  }}>
+                  Sign in with Google
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                padding: 20,
+              }}>
+              <Text>Don't have an account? </Text>
+              <TouchableOpacity
+                onPress={() => {
+                  alert('Navigate to Sigup');
+                }}>
+                <Text
+                  style={{
+                    color: '#5297fe',
+                    textDecorationLine: 'underline',
+                  }}>
+                  {' '}
+                  Sign up
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
---
+++ components/screens/login/style.js
@@ -0,0 +1,58 @@
+import {Platform, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e4f1f9',
+    paddingHorizontal: 24,
+  },
+  mid: {
+    flex: 80,
+    backgroundColor: 'pink',
+  },
+  title: {
+    fontSize: 28,
+    color: '#060302',
+    fontFamily: 'Montserrat-Medium',
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 12,
+  },
+  describe: {
+    fontSize: 14,
+    color: '#767372',
+    fontFamily: 'Montserrat-Medium',
+    textAlign: 'center',
+    marginBottom: 38,
+  },
+  inputField: {
+    height: Platform.OS === 'ios' ? 40 : 52,
+    width: '100%',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    paddingLeft: 56,
+    paddingRight: 32,
+    fontSize: 15,
+    fontFamily: 'Comfortaa-Bold',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  inputIcon: {
+    position: 'absolute',
+    top: '14%',
+    left: '4%',
+  },
+  buttonSignIn: {
+    width: '100%',
+    height: Platform.OS === 'ios' ? 40 : 52,
+    borderRadius: 28,
+    justifyContent: 'center',
+    alignItem: 'center',
+    backgroundColor: '#3787ff',
+    marginBottom: 28,
+  },
+});
+
+export default styles;
