ad65b830f3d7f09a618e83220f7281c6ab003946
Thành Tấn Trương <64310557+truongtanthanh1202@users.noreply.github.com>
2023-04-16 15:19:35 +0700

    Update index.js

--- components/screens/profile/profile_1/index.js
+++ components/screens/profile/profile_1/index.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, SafeAreaView} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import styles from './style';
 
 const Profile = ({route, navigation}) => {
   const {role} = route.params;
   const {email} = route.params;
   const {password} = route.params;
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#e4f1f9',
-      }}>
+    <SafeAreaView style={styles.container}>
       <Ionicons name="person-outline" size={32} />
       <Text>Profile {role}</Text>
       <Text>{email}</Text>
       <Text>{password}</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
---
+++ components/screens/profile/profile_1/style.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e4f1f9',
+  },
+});
+
+export default styles;
