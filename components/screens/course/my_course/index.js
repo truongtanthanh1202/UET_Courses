e53f6694a7eba98eab8a08c2103f4623315c58d1
nullchilly <nullchilly@gmail.com>
2023-04-18 17:07:26 +0700

    feat: course page

---
+++ components/screens/course/my_course/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+const MyCourse = () => {
+  return (
+    <View>
+      <Text>
+        Hello World
+      </Text>
+    </View>
+  );
+}
+
+export default MyCourse;
---
+++ components/screens/course/schedule/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+const Schedule = () => {
+  return (
+    <View>
+      <Text>
+        Hello Schedule
+      </Text>
+    </View>
+  );
+}
+
+export default Schedule;
