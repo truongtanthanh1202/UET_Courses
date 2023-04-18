b3d73c5e61a72e14b9acf7c494e36a7d82655785
nullchilly <nullchilly@gmail.com>
2023-04-18 17:07:26 +0700

    feat: course page

--- components/screens/course/index.js
+++ components/screens/course/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import {Text, View, Image} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import MyCourse from './my_course';
+import Schedule from './schedule';
+
+const Tab = createMaterialTopTabNavigator();
 
 const Course = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#e4f1f9',
-      }}>
-      <Ionicons name="terminal-outline" size={32} />
-      <Text>Course</Text>
-    </View>
+    // <View
+    //   style={{
+    //     flex: 1,
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     backgroundColor: '#e4f1f9',
+    //   }}>
+    //   <Ionicons name="terminal-outline" size={32} />
+    //   <Text>Course</Text>
+    // </View>
+    <Tab.Navigator tabBarPosition='top'>
+        <Tab.Screen name="MyCourse" component={MyCourse} />
+        <Tab.Screen name="Schedule" component={Schedule} />
+    </Tab.Navigator>
   );
 };
 
